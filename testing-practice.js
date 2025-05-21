00d640b2fa7f1c39babb9826f54375a921a03181
Hilman Fikry <hilman12fikry@gmail.com>
2025-05-21 21:28:26 +0700

    feat: set up testing and complete capitalize fn

---
+++ testing-practice.js
@@ -0,0 +1,9 @@
+function testingPractice() {
+  function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+  
+  return { capitalize }
+}
+
+export default testingPractice;
