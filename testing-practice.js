f4974e5c8a6721cf05a6c7aa57e8a4cdd85db43a
Hilman Fikry <hilman12fikry@gmail.com>
2025-05-22 13:20:10 +0700

    feat: complete all testingPractice functions

--- testing-practice.js
+++ testing-practice.js
@@ -3,7 +3,72 @@ function testingPractice() {
     return string[0].toUpperCase() + string.slice(1);
   }
   
-  return { capitalize }
+  function reverseString(string) {
+    let reversedString = "";
+    for(let index = 0; index < string.length; index++) {
+      reversedString += string[string.length - 1 - index];
+    }
+    return reversedString;
+  }
+
+  const calculator = {
+    add(firstNum, secondNum) {
+      return firstNum + secondNum;
+    },
+    subtract(firstNum, secondNum) {
+      return firstNum - secondNum;
+    },
+    divide(firstNum, secondNum) {
+      return firstNum / secondNum;
+    },
+    multiply(firstNum, secondNum) {
+      return firstNum * secondNum;
+    }  
+  }
+
+  function caesarCipher(string, shiftFactor) {
+    let chiperedString = "";
+    for(let index = 0; index < string.length; index++) {
+      chiperedString += shiftChar(string[index], shiftFactor, categorizeChar(string[index]));
+    }
+    return chiperedString;
+  }
+
+  function categorizeChar(char) {
+    const charCode = char.codePointAt(0);
+    if(charCode >= 65 && charCode <= 90) return "uppercase";
+    else if(charCode >= 97 && charCode <= 122) return "lowercase";
+    else return "others";
+  }
+
+  function shiftChar(char, shiftFactor, category) {
+    let charCode = char.codePointAt(0);
+    switch(category) {
+      case "uppercase":
+        charCode += shiftFactor;
+        if(charCode < 65) return String.fromCodePoint(charCode + 26);
+        else if(charCode > 90) return String.fromCodePoint(charCode - 26);
+        else return String.fromCodePoint(charCode);
+      case "lowercase":
+        charCode += shiftFactor;
+        if(charCode < 97) return String.fromCodePoint(charCode + 26);
+        else if(charCode > 122) return String.fromCodePoint(charCode - 26);
+        else return String.fromCodePoint(charCode);
+      case "others":
+        return String.fromCodePoint(charCode);
+    }
+  }
+
+  function analyzeArray(array) {
+    return {
+      average: array.reduce((total, current) => total + current) / array.length,
+      min: Math.min(...array),
+      max: Math.max(...array),
+      length: array.length
+    };
+  }
+
+  return { capitalize, reverseString, calculator, caesarCipher, analyzeArray }
 }
 
 export default testingPractice;
--- testing-practice.test.js
+++ testing-practice.test.js
@@ -14,3 +14,47 @@ describe("capitalize function", () => {
     expect(testing.capitalize("Hilman")).toBe("Hilman");
   });
 });
+
+test("reverse string", () => {
+  expect(testing.reverseString("Ciau bella, Hilman!"))
+    .toBe("!namliH ,alleb uaiC");
+});
+
+describe("calculator object methods", () => {
+  test("add", () => {
+    expect(testing.calculator.add(-1, 10)).toBe(9);
+  });
+  test("subtract", () => {
+    expect(testing.calculator.subtract(-1, -10)).toBe(9);
+  });
+  test("divide", () => {
+    expect(testing.calculator.divide(-10, 2)).toBe(-5);
+  });
+  test("multiply", () => {
+    expect(testing.calculator.multiply(-2, 10)).toBe(-20);
+  });    
+});
+
+describe("caesarCipher function", () => {
+  test("vanilla", () => {
+    expect(testing.caesarCipher("xyz", 3)).toBe("abc");
+  });
+  test("case preservation", () => {
+    expect(testing.caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  });
+  test("punctuation preservation", () => {
+    expect(testing.caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+  });
+  test("outside range shift", () => {
+    expect(testing.caesarCipher("a", -3)).toBe("x");
+  })
+});
+
+test("analyzeArray function", () => {
+  expect(testing.analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  });
+});
